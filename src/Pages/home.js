e664487616575544913b4ef22afc2d50936976b1
Yoshihara, Sean Tsubasa <syoshihara3@gatech.edu>
2023-07-04 15:47:58 +0200

    pray

---
+++ src/Pages/home.js
@@ -0,0 +1,22 @@
+import React from 'react';
+function Home() {
+    return (
+        <main>
+            <section>
+                <h1>
+                    Changing how we think about code through AI computing
+                    {/*<div className="scrollDown">*/}
+                    {/*    V*/}
+                    {/*</div>*/}
+                </h1>
+                <a href="https://forms.gle/g378D2EMSFTQ3bjd8" target="_blank">
+                    <button className="waitlistButton">
+                        Join the Waitlist
+                    </button>
+                </a>
+            </section>
+        </main>
+    );
+};
+
+export default Home;
